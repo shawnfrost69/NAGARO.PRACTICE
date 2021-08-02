1a2ae54f75142153e05b037ba855a10c0f4e6382
Akash Verma <65110494+shawnfrost69@users.noreply.github.com>
2021-08-03 02:33:23 +0530

    Add files via upload

--- PRACTICE/src/com/company/MaxSubArray.java
+++ PRACTICE/src/com/company/MaxSubArray.java
@@ -1,2 +1,23 @@
-package com.company;public class MaxSubArray {
+package com.company;
+
+import java.util.Scanner;
+
+public class MaxSubArray {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int [] arr = new int[n];
+        for (int i =0; i <arr.length; i++){
+            arr[i] = scn.nextInt();
+        }
+        int max =arr[0];
+        int sum =0;
+
+        for (int i =0; i <arr.length; i++){
+            sum +=arr[i];
+            if (sum > max) max =sum;
+            if (sum < 0) sum =0;
+        }
+        System.out.println(max);
+    }
 }
